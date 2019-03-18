801c037cb89aa5491c6e11c5f0388ee581b6308b
Fdim_ <Fdim_@DESKTOP-5GCKDTM>
2019-03-18 20:38:46 +0200

    15_3_2019

---
+++ src/pkg15_3_2019/New_class_38.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pkg15_3_2019;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Fdim_
+ */
+public class New_class_38 {
+    
+    
+    
+    
+      
+    
+    
+}
